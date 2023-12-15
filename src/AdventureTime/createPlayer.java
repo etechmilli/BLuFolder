3f6f6263f6e14e237bd0d92b8631eead9cba33f0
ETech <Etech@gmail.com>
2023-12-15 13:00:38 -0500

    first commit

---
+++ src/AdventureTime/createPlayer.java
@@ -0,0 +1,23 @@
+//package AdventureTime;
+//
+//public static Player createPlayer(Scanner scanner) {
+//    System.out.println("Create your player:");
+//    System.out.print("Enter Health Points (HP): ");
+//    int hp = scanner.nextInt();
+//
+//    System.out.print("Enter Strength: ");
+//    int strength = scanner.nextInt();
+//
+//    System.out.print("Enter Wisdom: ");
+//    int wisdom = scanner.nextInt();
+//
+//    System.out.print("Enter Magic: ");
+//    int magic = scanner.nextInt();
+//
+//    System.out.print("Enter Experience Level: ");
+//    int experienceLevel = scanner.nextInt();
+//
+//    // Create and return the player object with the provided values
+//    return new Player(hp, strength, wisdom, magic, experienceLevel);
+//}
+//}
